e37c56e1ddc7493cf1d82bab97b86df47683ca7a
harshgurjar17 <harshgurjar4544@gmail.com>
2024-10-25 18:56:16 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,16 @@
+function validate() {
+    var email = document.myform.email.value;
+    if (email == null || email == "") {
+        alert("Please Enter Your Email");
+        return false;
+    }
+
+    var password = document.myform.password.value;
+    if (password == null || password == "") {
+        alert("Please Enter Your Password");
+        return false;
+    }
+
+    alert("Server is not active!!")
+    return false;
+}
